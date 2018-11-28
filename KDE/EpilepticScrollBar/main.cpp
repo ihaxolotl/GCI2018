1b049541b5ec198e8c4bc1e185cc34d630eb860d
Brett Broadhurst <42479242+brettbroadhurst@users.noreply.github.com>
2018-11-28 04:49:33 -0800

    Fixed indents.

--- KDE/EpilepticScrollBar/main.cpp
+++ KDE/EpilepticScrollBar/main.cpp
@@ -9,23 +9,23 @@
 // I wish for a centered window.
 int main(int argc, char **argv)
 {
-  QApplication app(argc, argv);
-  //QDesktopWidget wid;
-  MainWindow trippy;
+	QApplication app(argc, argv);
+  	//QDesktopWidget wid;
+  	MainWindow trippy;
 
-  /* I don't know what exploded here!!!!
-  int width = trippy.frameGeometry().width();
-  int height = trippy.frameGeometry().height();
-  int screenWidth = wid.screen()->width();
-  int screenHeight = wid.screen()->height();
+  	/* I don't know what exploded here!!!!
+ 	int width = trippy.frameGeometry().width();
+  	int height = trippy.frameGeometry().height();
+  	int screenWidth = wid.screen()->width();
+  	int screenHeight = wid.screen()->height();
   
-  trippy.setGeometry((screenWidth / 2) - (width / 2),
+  	trippy.setGeometry((screenWidth / 2) - (width / 2),
 		     (screenHeight / 2) - (height / 2),
 		     width, height);
   */
-  trippy.resize(320, 240);
-  trippy.setWindowTitle("This was not my fault...");
-  trippy.show();
+  	trippy.resize(320, 240);
+  	trippy.setWindowTitle("This was not my fault...");
+  	trippy.show();
   
-  return app.exec();
+  	return app.exec();
 }
